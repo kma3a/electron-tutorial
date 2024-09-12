870e5200ea769eba83c0b82d4a77b9b1eabbf67a
Kelsey A <d.chan42@gmail.com>
2024-09-12 18:08:32 -0400

    Finishing the preload scripts portion of tutorial

---
+++ preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+  // never expose this directly because it can be an attack vector for malicious code
+  // we can also expose variables, not just functions
+});
